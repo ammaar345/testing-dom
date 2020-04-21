0a00bad44af14960cc2ab627543fe5d9164a8c1f
Ammaar <fakirammaar8@gmail.com>
2020-04-21 11:45:41 +0200

    .

--- settings-bill-use.test.js
+++ settings-bill-use.test.js
@@ -1,34 +1,34 @@
-describe("use values",function(){
-    it("should be able to use the set call cost.",function(){
-        let settingsBill=BillWithSettings();
+describe("use values", function () {
+    it("should be able to use the set call cost.", function () {
+        let settingsBill = BillWithSettings();
         settingsBill.setCallCost(2.25);
         settingsBill.setSmsCost(0.85);
         settingsBill.makeCall();
         settingsBill.makeCall();
         settingsBill.makeCall();
-        assert.equal(6.75,settingsBill.getTotalCost());
-        assert.equal(6.75,settingsBill.getTotalCallCost());
-        assert.equal(0,settingsBill.getTotalSmsCost());
-    })
-    it("should be able to use the call cost set for 2 calls at R1.35 each. ",function(){
-        let settingsBill=BillWithSettings();
+        assert.equal(6.75, settingsBill.getTotalCost());
+        assert.equal(6.75, settingsBill.getTotalCallCost());
+        assert.equal(0, settingsBill.getTotalSmsCost());
+    });
+    it("should be able to use the call cost set for 2 calls at R1.35 each. ", function () {
+        let settingsBill = BillWithSettings();
         settingsBill.setCallCost(1.35);
         settingsBill.setSmsCost(0.85);
         settingsBill.makeCall();
         settingsBill.makeCall();
-        assert.equal(2.70,settingsBill.getTotalCost());
-        assert.equal(2.70,settingsBill.getTotalCallCost());
-        assert.equal(0,settingsBill.getTotalSmsCost());
-    })
-    it("should be able to send 2 sms's at R0.85 each and make 1 call at R1.35. ",function(){
-        let settingsBill=BillWithSettings();
+        assert.equal(2.70, settingsBill.getTotalCost());
+        assert.equal(2.70, settingsBill.getTotalCallCost());
+        assert.equal(0, settingsBill.getTotalSmsCost());
+    });
+    it("should be able to send 2 sms's at R0.85 each and make 1 call at R1.35. ", function () {
+        let settingsBill = BillWithSettings();
         settingsBill.setCallCost(1.35);
         settingsBill.setSmsCost(0.85);
-settingsBill.makeCall();
+        settingsBill.makeCall();
         settingsBill.sendSms();
         settingsBill.sendSms();
-        assert.equal(3.05,settingsBill.getTotalCost());
-        assert.equal(1.35,settingsBill.getTotalCallCost());
-        assert.equal(1.70,settingsBill.getTotalSmsCost());
-    })
+        assert.equal(3.05, settingsBill.getTotalCost());
+        assert.equal(1.35, settingsBill.getTotalCallCost());
+        assert.equal(1.70, settingsBill.getTotalSmsCost());
+    });
 })
--- settings-bill.js
+++ settings-bill.js
@@ -58,7 +58,7 @@ function BillWithSettings(){
        sendSms,
        getTotalCost,
         getTotalSmsCost,
-        getTotalCallCost,
+        getTotalCallCost
            
         }
     }
